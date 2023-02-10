8f7ef59b0250b2faa254dfb3f131c093121920f0
Leonardodarienzo <darienzo.leonardo@iisgalvanimi.edu.it>
2023-02-10 12:36:49 +0000

    primo commit

--- src/app/mock-cars.ts
+++ src/app/mock-cars.ts
@@ -1,9 +1,9 @@
 import { Car } from './car';
 
 export const CARS: Car[] = [
-  { id: 11, name: 'Putno', costo:10, consumo:25 },
-  { id: 12, name: 'Bravo', costo:20, consumo:20 }, 
-  { id: 13, name: 'Tipo', costo:15, consumo:10 }, 
-  { id: 14, name: '124_spider' , costo:100, consumo:90 },
-  { id: 15, name: '500', costo:1000, consumo:33 }
+  { id: 11, name: 'Ferrari', costo:10, consumo:25 },
+  { id: 12, name: 'Peugeot', costo:20, consumo:20 }, 
+  { id: 13, name: 'Fait Panda', costo:15, consumo:10 }, 
+  { id: 14, name: 'Gtr' , costo:100, consumo:90 },
+  { id: 15, name: 'Jeep', costo:1000, consumo:33 }
 ];
